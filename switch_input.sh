a8ac8ab4c3a499bbf81ce535869e52395ffa3878
Lauren Lajas <29675370+llajas@users.noreply.github.com>
2023-02-12 23:48:05 -0600

    Updated script - Outputs Current Device

--- switch_input.sh
+++ switch_input.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set +x
 
 # Check for correct number of arguments
 if [ $# -lt 2 ] || [ $# -gt 3 ]; then
@@ -11,9 +12,21 @@ IP_ADDRESS=$1
 OPTION=$2
 PORT_NUMBER=$3
 
+# Load device configuration
+CONFIG_FILE="devices.conf"
+declare -A DEVICES
+while IFS== read -r port device
+do
+  if [[ -n "$port" ]]; then
+    DEVICES["$port"]=$device
+  fi
+done < "$CONFIG_FILE"
+
 # Parse command line options
 case $OPTION in
-  -g|-get) SWITCH_COMMAND="read" ;;
+  -g|-get)
+    SWITCH_COMMAND="read"
+    ;;
   -s|-set)
     if [[ -z $PORT_NUMBER ]]; then
       echo "Error: Port number is required for -set option."
@@ -25,8 +38,12 @@ case $OPTION in
     fi
     SWITCH_COMMAND="sw i0$PORT_NUMBER"
     ;;
-  -n|-next) SWITCH_COMMAND="sw +" ;;
-  -p|-previous) SWITCH_COMMAND="sw -" ;;
+  -n|-next)
+    SWITCH_COMMAND="sw +"
+    ;;
+  -p|-previous)
+    SWITCH_COMMAND="sw -"
+    ;;
   *)
     echo "Error: Invalid option. Use -get, -set, -next, or -previous."
     exit 1
@@ -35,8 +52,18 @@ esac
 
 # Send switch command to converter
 if echo -e "$SWITCH_COMMAND\r" | nc -w 2 -z $IP_ADDRESS 4999 > /dev/null 2>&1; then
-  if echo -e "$SWITCH_COMMAND\r" | nc -w 2 $IP_ADDRESS 4999 ; then
-    :
+  if response=$(echo -e "$SWITCH_COMMAND\r" | nc -w 2 $IP_ADDRESS 4999); then
+    if [[ $OPTION == "-g" ]]; then
+      port=$(echo "$response" | awk -F: '/Input/ {gsub(/[^0-9]/,"",$2); print $2}')
+      DEVICE=${DEVICES[$port]}
+      if [[ -n $DEVICE ]]; then
+        echo "The $DEVICE is currently selected."
+      else
+        echo "There is no device set up or configured on the selected port."
+      fi
+    else
+      echo "Switch command response: $response"
+    fi
   else
     echo "Error: Failed to execute switch command."
     exit 1
@@ -45,4 +72,3 @@ else
   echo "Error: Failed to connect to device."
   exit 1
 fi
-
